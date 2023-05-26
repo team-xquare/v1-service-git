e42a8db8bce6f69a0c30ca58c23d1f03d122498d
jaemin <sonjm3518@dsm.hs.kr>
2023-05-26 14:19:41 +0900

    📑 :: 전체 깃 정보 반환

---
+++ git-application/src/main/kotlin/com/xquare/git/git/usecase/FindAllGit.kt
@@ -0,0 +1,29 @@
+package com.xquare.git.git.usecase
+
+import com.xquare.git.annotations.UseCase
+import com.xquare.git.git.dto.FindAllUserResponse
+import com.xquare.git.git.dto.FindUserElement
+import com.xquare.git.git.spi.QueryGitPort
+import com.xquare.git.user.spi.QueryUserPort
+
+@UseCase
+class FindAllGit(
+    private val queryGitPort: QueryGitPort,
+    private val queryUserPort: QueryUserPort
+) {
+    suspend fun execute(): FindAllUserResponse {
+        val response = queryGitPort.getAllGit()
+            .map {
+                val name = queryUserPort.getName(it.userId).name
+                FindUserElement(
+                    userId = it.userId,
+                    name = name,
+                    username = it.username,
+                    avatarUrl = it.avatarUrl,
+                    contributions = it.contributions
+                )
+            }
+
+        return FindAllUserResponse(response)
+    }
+}
