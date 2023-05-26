e7ab67182cd7e691d93a95bf2605a3996ef5af0c
jaemin <sonjm3518@dsm.hs.kr>
2023-05-26 13:56:49 +0900

    📑 :: Git Router

---
+++ git-presentation/src/main/kotlin/com/xquare/git/git/GitHandler.kt
@@ -0,0 +1,40 @@
+package com.xquare.git.git
+
+import com.xquare.git.git.dto.FindAllUserResponse
+import com.xquare.git.git.dto.FindUserElement
+import com.xquare.git.git.facade.GitFacade
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.bodyValueAndAwait
+import org.springframework.web.reactive.function.server.buildAndAwait
+import java.net.URI
+
+@Component
+class GitHandler(
+    private val gitFacade: GitFacade,
+    ) {
+    suspend fun saveUsername(serverRequest: ServerRequest): ServerResponse {
+        val userId = serverRequest.headers().firstHeader("Request-User-Id")
+        val username = serverRequest.queryParam("username").orElse( "")
+        gitFacade.saveUsername(userId, username)
+
+        return ServerResponse.created(URI("/gits")).buildAndAwait()
+    }
+
+    suspend fun getAllGit(serverRequest: ServerRequest): ServerResponse {
+        val gitResponse: FindAllUserResponse = gitFacade.findAllGit()
+        return ServerResponse.ok().bodyValueAndAwait(gitResponse)
+    }
+
+    suspend fun getCurrentGit(serverRequest: ServerRequest): ServerResponse {
+        val userId = serverRequest.headers().firstHeader("Request-User-Id")
+        val gitResponse: FindUserElement = gitFacade.findGitByCurrentUserId(userId)
+        return ServerResponse.ok().bodyValueAndAwait(gitResponse)
+    }
+
+    suspend fun updateContributions(serverRequest: ServerRequest): ServerResponse {
+        gitFacade.updateContributions()
+        return ServerResponse.noContent().buildAndAwait()
+    }
+}
