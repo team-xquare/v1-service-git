4eb0ef485d9f47b4ff5172923825a0ad782b6d2c
jaemin <sonjm3518@dsm.hs.kr>
2023-05-26 14:04:43 +0900

    📑 :: Git 예외처리

---
+++ git-application/src/main/kotlin/com/xquare/git/git/exceptions/GitExceptions.kt
@@ -0,0 +1,18 @@
+package com.xquare.git.git.exceptions
+
+import com.xquare.git.exceptions.BaseException
+
+sealed class GitExceptions(
+    override val statusCode: Int,
+    override val errorMessage: String
+) : BaseException(statusCode, errorMessage) {
+
+    class NotFound(message: String = NOT_FOUND) : GitExceptions(404, message)
+
+    class AlreadyExists(message: String = ALREADY_EXISTS) : GitExceptions(409, message)
+
+    companion object {
+        private const val NOT_FOUND = "Not Found"
+        private const val ALREADY_EXISTS = "Already Exists"
+    }
+}
