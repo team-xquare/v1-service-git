6108307ce051663ec6997307a1a9d66644cca0af
jaemin <sonjm3518@dsm.hs.kr>
2023-05-26 10:32:25 +0900

    📑 :: Security 설정

---
+++ git-infrastructure/src/main/kotlin/com/xquare/git/global/config/SecurityConfiguration.kt
@@ -0,0 +1,22 @@
+package com.xquare.git.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@EnableWebFluxSecurity
+class SecurityConfiguration {
+    @Bean
+    protected fun filterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http
+            .httpBasic().disable()
+            .formLogin().disable()
+            .csrf().disable()
+            .cors().disable()
+            .authorizeExchange()
+            .anyExchange().permitAll()
+            .and().build()
+    }
+}
