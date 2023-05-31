24e53277c447040605ee7e7e6246638cdaa10644
jaemin <sonjm3518@dsm.hs.kr>
2023-05-31 22:29:04 +0900

    ♻️  :: rollBackFor 제거

--- git-application/src/main/kotlin/com/xquare/git/git/facade/GitFacade.kt
+++ git-application/src/main/kotlin/com/xquare/git/git/facade/GitFacade.kt
@@ -2,10 +2,10 @@ package com.xquare.git.git.facade
 
 import com.xquare.git.git.dto.FindAllUserResponse
 import com.xquare.git.git.dto.FindUserElement
-import com.xquare.git.git.usecase.CheckUsername
+import com.xquare.git.git.usecase.CheckUsernameUseCase
 import com.xquare.git.git.usecase.FindAllGit
 import com.xquare.git.git.usecase.FindGitByCurrentUserId
-import com.xquare.git.git.usecase.SaveUsername
+import com.xquare.git.git.usecase.SaveUsernameUseCase
 import com.xquare.git.git.usecase.UpdateGit
 import com.xquare.git.user.usecase.GetUserId
 import org.springframework.stereotype.Component
@@ -13,32 +13,32 @@ import org.springframework.transaction.annotation.Transactional
 
 @Component
 open class GitFacade(
-    private val saveUsername: SaveUsername,
-    private val checkUsername: CheckUsername,
+    private val saveUsernameUseCase: SaveUsernameUseCase,
+    private val checkUsernameUseCase: CheckUsernameUseCase,
     private val findAllGit: FindAllGit,
     private val updateGit: UpdateGit,
     private val getUserId: GetUserId,
     private val findGitByCurrentUserId: FindGitByCurrentUserId
 ) {
-    @Transactional(rollbackFor = [Exception::class])
+    @Transactional
     open suspend fun saveUsername(userId: String?, username: String) {
-        checkUsername.execute(username)
+        checkUsernameUseCase.execute(username)
         val uuid = getUserId.execute(userId)
-        saveUsername.execute(uuid, username)
+        saveUsernameUseCase.execute(uuid, username)
     }
 
-    @Transactional(rollbackFor = [Exception::class])
+    @Transactional
     open suspend fun findAllGit(): FindAllUserResponse {
         return findAllGit.execute()
     }
 
-    @Transactional(rollbackFor = [Exception::class])
+    @Transactional
     open suspend fun findGitByCurrentUserId(userId: String?): FindUserElement {
         val uuid = getUserId.execute(userId)
         return findGitByCurrentUserId.execute(uuid)
     }
 
-    @Transactional(rollbackFor = [Exception::class])
+    @Transactional
     open suspend fun updateContributions() {
         updateGit.execute()
     }
