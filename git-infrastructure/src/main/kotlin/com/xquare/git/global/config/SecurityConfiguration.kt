f345266e9eb3ef47d861d4a1d27f30cf50141d1e
jaemin <sonjm3518@dsm.hs.kr>
2023-06-29 12:31:37 +0900

    ♻️ :: Configuration 추가

--- git-infrastructure/src/main/kotlin/com/xquare/git/global/config/SecurityConfiguration.kt
+++ git-infrastructure/src/main/kotlin/com/xquare/git/global/config/SecurityConfiguration.kt
@@ -1,10 +1,12 @@
 package com.xquare.git.global.config
 
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
 
+@Configuration
 @EnableWebFluxSecurity
 class SecurityConfiguration {
     @Bean
