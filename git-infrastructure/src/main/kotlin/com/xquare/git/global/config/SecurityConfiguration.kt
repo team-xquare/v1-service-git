f538c518aeabbc21e499132f6c7eb0a5823784f4
jaemin <sonjm3518@dsm.hs.kr>
2023-06-30 13:59:02 +0900

    ♻️ :: authenticationEntryPoint 설정

--- git-infrastructure/src/main/kotlin/com/xquare/git/global/config/SecurityConfiguration.kt
+++ git-infrastructure/src/main/kotlin/com/xquare/git/global/config/SecurityConfiguration.kt
@@ -2,9 +2,11 @@ package com.xquare.git.global.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint
 
 @Configuration
 @EnableWebFluxSecurity
@@ -12,12 +14,15 @@ class SecurityConfiguration {
     @Bean
     protected fun filterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
         return http
-            .httpBasic().disable()
             .formLogin().disable()
             .csrf().disable()
             .cors().disable()
             .authorizeExchange()
-            .anyExchange().permitAll()
-            .and().build()
+            .anyExchange().authenticated()
+            .and()
+            .httpBasic()
+            .authenticationEntryPoint(HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED))
+            .and()
+            .build()
     }
 }
