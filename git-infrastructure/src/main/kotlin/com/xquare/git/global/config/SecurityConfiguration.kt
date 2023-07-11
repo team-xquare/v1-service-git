9cbac3c5a68ad07144ddece88bd78c952a202793
jaemin <sonjm3518@dsm.hs.kr>
2023-07-11 23:39:04 +0900

    ♻️ :: authenticationEntryPoint 수정

--- git-infrastructure/src/main/kotlin/com/xquare/git/global/config/SecurityConfiguration.kt
+++ git-infrastructure/src/main/kotlin/com/xquare/git/global/config/SecurityConfiguration.kt
@@ -5,8 +5,12 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.server.SecurityWebFilterChain
-import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint
+import org.springframework.security.web.server.ServerAuthenticationEntryPoint
+import org.springframework.stereotype.Component
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
 
 @Configuration
 @EnableWebFluxSecurity
@@ -21,8 +25,16 @@ class SecurityConfiguration {
             .anyExchange().authenticated()
             .and()
             .httpBasic()
-            .authenticationEntryPoint(HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED))
+            .authenticationEntryPoint(CustomAuthenticationEntryPoint())
             .and()
             .build()
+    }
+}
+
+@Component
+class CustomAuthenticationEntryPoint : ServerAuthenticationEntryPoint {
+    override fun commence(exchange: ServerWebExchange, ex: AuthenticationException): Mono<Void> {
+        exchange.response.statusCode = HttpStatus.UNAUTHORIZED
+        return exchange.response.setComplete()
     }
 }
