427a105f48b4b6574f0299a2d5ec78efaf828cb6
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-07-12 11:55:26 +0900

    ♻️ :: 패키지 명 수정

--- git-infrastructure/src/main/kotlin/com/xquare/v1servicegit/GitApplication.kt
+++ git-infrastructure/src/main/kotlin/com/xquare/v1servicegit/GitApplication.kt
@@ -1,9 +1,10 @@
-package com.xquare.git
+package com.xquare.v1servicegit
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
-internal const val BASE_PACKAGE = "com.xquare.git"
+internal const val BASE_PACKAGE = "com.xquare.v1servicegit"
 
 @SpringBootApplication
 class GitApplication
--- git-infrastructure/src/main/kotlin/com/xquare/v1servicegit/router/GitRouter.kt
+++ git-infrastructure/src/main/kotlin/com/xquare/v1servicegit/router/GitRouter.kt
@@ -1,4 +1,4 @@
-package com.xquare.git
+package com.xquare.v1servicegit.router
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
